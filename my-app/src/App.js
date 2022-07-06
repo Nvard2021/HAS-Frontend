76db62f1ea2e157fa89f798b9eea4e6cc278d652
Nvard <nvard.badalyan.im@gmail.com>
2022-07-06 15:09:42 +0400

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,19 +7,31 @@ import Bar from "./pages/pages/Bar";
 import Kitchen from "../src/pages/Kitchen";
 import MusicHall from "../src/pages/MusicHall";
 import Events from "../src/pages/Events";
+import ROUTES from "../src/constants/Routes";
 
+// const withPrivateRoute = ({ user, redirectPath = '/home' }) => {
+//   if (!user) {
+//     return <Navigate to={redirectPath} replace />;
+//   }
+
+//   return <Login />;
+// };
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/restaurant" element={<Restaurant />}></Route>
-        <Route exact path="/bar" element={<Bar />}></Route>
-        <Route exact path="/kitchen" element={<Kitchen />}></Route>
-        <Route exact path="/musichall" element={<MusicHall />}></Route>
-        <Route exact path="/events" element={<Events />}></Route>
+        <Route exact path={ROUTES.Home} element={<Home />}></Route>
+        <Route path={ROUTES.LOGIN} element={<Login />}></Route>
+        <Route path={ROUTES.Restaurant} element={<Restaurant />}></Route>
+        <Route path={ROUTES.Bar} element={<Bar />}></Route>
+        <Route path={ROUTES.Kitchen} element={<Kitchen />}></Route>
+        <Route path={ROUTES.MusicHall} element={<MusicHall />}></Route>
+        <Route path={ROUTES.Events} element={<Events />}></Route>
+        <Route path={ROUTES.NotFound} element={<Error404 />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
+
+
---
+++ my-app/src/constants/Routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const ROUTES = {
+    Login: "/login",
+    Home: "/home",
+    Restaurant: "/restaurant",
+    Bar: "/bar",
+    Kitchen: "/kitchen",
+    MusicHall: "/musichall",
+    Events: "/events",
+    NotFound: "/error404"
+}
+
+export default ROUTES;
+
