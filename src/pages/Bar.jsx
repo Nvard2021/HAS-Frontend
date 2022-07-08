5da9e0b1466ce58fb9215bc8637671288214b5b0
musho04 <mushegh.esayan.im@gmail.com>
2022-07-08 14:34:52 +0400

    test

--- src/pages/Bar.jsx
+++ src/pages/Bar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+console.log('asasa');
 
 export default function Bar() {
   return (
     <div>
       <h2>Bar
-        page  0<;/h2>
+        page  0;</h2>
     </div>
   );
 }
